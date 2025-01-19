717ca448aedba826fdef982a5c36a5a920da83c0
Erkhan Kubatbaev <alp-erkhan.kubatbaev@mail.schwarz>
2025-01-19 11:32:02 +0100

    Add created_at field to the BlogPost struct

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,9 +1,14 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type BlogPost struct {
-	ID      uuid.UUID `json:"id"`
-	Title   string    `json:"title"`
-	Content string    `json:"content"`
+	ID        uuid.UUID `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
